7f9b3740e04c678184218cd842e827314e5a98a8
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-04-21 03:38:45 +0100

    Updated error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,12 +4,31 @@
       class="d-flex justify-content-center align-items-center min-vh-100 border-dark border-bottom text -center p-2 m-0 my-auto"
       style="background-image: url('/patterns/stardust.png')"
     >
-      <div class="display-3 text-center">
-        Oops!
-        <br />That was an error.
-        <br />
-        <i class="fa fa-frown-o"></i>
+      <div>
+        <div class="display-3 text-center">{{error.statusCode}} Error!</div>
+        <div class="display-4 text-center">{{error.message}}</div>
+
+        <div class="display-1git text-center">
+          <i class="fa fa-frown-o"></i>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  head() {
+    return {
+      title: "Error - OgbeniHMMD"
+    };
+  }
+};
+</script>
