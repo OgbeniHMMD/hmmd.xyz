98d7444fe951822a787360fffe87f71470388f02
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-04-21 14:35:03 +0100

    Added Telegram support to the contact form

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -1,46 +1,49 @@
 <template>
   <div id="contact">
+    <a name="contact"></a>
     <div
       class="d-flex justify-content-center align-items-center min-vh-100 border-dark border-bottom text -center p-2 m-0 my-auto"
       style="background-image: url('/patterns/white-texture.png')"
     >
-      <form class="container">
+      <div class="container">
         <div class="row">
           <h1 class="col-12 mb-4">Contact Me</h1>
 
           <div
-            class="col-12 alert alert-warning alert-dismissible fade show text-center"
             role="alert"
+            v-if="alertType"
+            :class="'alert-' + alertType"
+            class="col-12 alert alert-dismissible fade show text-center"
           >
-            <strong>NB:</strong> This feature is not working yet. Please check back later.
-          </div>
-
-          <div
-            role="alert"
-            v-if="alert "
-            class="col-12 alert alert-success alert-dismissible fade show text-center"
-          >
-            {{errMsg}}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+            <span v-html="alertMsg"></span>
           </div>
 
           <div class="col-12 col-md-6">
-            <input class="form-control form-control-lg border-0 shadow mb-4" placeholder="Name" />
+            <input
+              id="name"
+              required
+              v-model.trim="name"
+              class="form-control form-control-lg border-0 shadow mb-4"
+              placeholder="Name"
+            />
           </div>
           <div class="col-12 col-md-6">
             <input
+              id="email"
+              required
+              type="email"
+              v-model.trim="email"
               class="form-control form-control-lg border-0 shadow mb-4"
               placeholder="E-mail@Example.com"
-              type="email"
             />
           </div>
           <div class="col-12">
             <textarea
-              class="form-control form-control-lg border-0 shadow mb-4"
-              placeholder="Your Message..."
               rows="4"
+              required
+              v-model.trim="textArea"
+              placeholder="Your Message..."
+              class="form-control form-control-lg border-0 shadow mb-4"
             ></textarea>
           </div>
         </div>
@@ -49,15 +52,12 @@
           <button
             type="reset"
             class="btn btn-lg btn-outline-danger col shadow mr-4"
-            v-on:click="alert=null"
+            v-on:click="reset()"
           >
             <i class="fa fa-times mr-4" />Reset
           </button>
 
-          <button
-            class="btn btn-lg btn-outline-primary col shadow"
-            v-on:click="showalert('success', 'Message sent...')"
-          >
+          <button class="btn btn-lg btn-outline-primary col shadow" v-on:click="validateData()">
             <i class="fa fa-envelope-o mr-4"></i>Send
           </button>
 
@@ -65,13 +65,13 @@
             <jump-down anchor="social" />
           </div>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
 
-
 <script>
+import axios from "axios";
 import JumpDown from "~/components/JumpDown.vue";
 
 export default {
@@ -80,17 +80,65 @@ export default {
   },
   data: function() {
     return {
-      alert: null,
-      errMsg: null
+      alertType: null,
+      alertMsg: null,
+
+      name: "",
+      email: "",
+      textArea: "",
+
+      chatId: "1229306973",
+      token: "bot1200246144:AAFgPUdih32F60oA-C-Ratzoxdqvm_7R6A8",
+      reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
     };
   },
+
   methods: {
-    removeAlert() {
-      this.alert = "";
+    reset() {
+      this.alerType = "";
+      this.alertMsg = "";
+
+      this.name = "";
+      this.email = "";
+      this.textArea = "";
     },
-    showalert(type, msg) {
-      this.alert = type;
-      this.errMsg = msg;
+    showAlert(type, msg) {
+      this.alertType = type;
+      this.alertMsg = msg;
+    },
+    validateData() {
+      if (this.name === "") {
+        this.showAlert("warning", "Please enter a name");
+      } else if (this.email === "" || !this.reg.test(this.email)) {
+        this.showAlert("warning", "Your e-mail is invalid");
+      } else if (this.textArea === "") {
+        this.showAlert("warning", "Please enter your message");
+      } else {
+        this.sendMsg();
+      }
+    },
+
+    async sendMsg() {
+      try {
+        await axios.get(
+          "https://api.telegram.org/" +
+            this.token +
+            "/sendMessage?chat_id=" +
+            this.chatId +
+            "&text=" +
+            encodeURIComponent(this.textArea) +
+            "&parse_mode=html"
+        );
+        this.showAlert("success", "Message sent. Thank you!");
+      } catch (e) {
+        console.log("An Error Occured: ");
+        this.showAlert(
+          "danger",
+          "Sorry, an error occured. <b>Your Message was not sent.</b>"
+        );
+      }
+
+      //https://api.telegram.org/bot1200246144:AAFgPUdih32F60oA-C-Ratzoxdqvm_7R6A8/getUpdates
     }
   }
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxtjs/pwa"],
+  modules: ["@nuxtjs/axios", "@nuxtjs/pwa"],
   /*
    ** Build configuration
    */
