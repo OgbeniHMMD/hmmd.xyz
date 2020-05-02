328c820ca2543aa8400df0ce45c65b1cd6dfab64
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-05-02 09:57:06 +0100

    Added'Scroll to anchor'

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,10 @@
+// scroll to anchor
+
+export default function(to, from, savedPosition) {
+  if (to.hash) {
+    return {
+      selector: to.hash
+      // , offset: { x: 0, y: 10 }
+    };
+  }
+}
