6a6dc390680ac4597b28d7a6db7dbf3f104d4207
-h <Olajide.A.Hammed@gmail.com>
2020-05-06 16:05:00 +0100

    Relaced links with icons

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,7 +4,7 @@
     class="d-flex justify-content-center align-items-center flex-column min-vh-100 border-primary border-bottom text-center p-2 m-0 my-auto"
     style="background-image: url('/patterns/subtle-grey.png')"
   >
-    <div class="w-100 p-3 text-primary">
+    <div class="w-100 p-2 text-primary">
       <img
         id="photo"
         src="~/assets/img/OgbeniHMMD.jpg"
@@ -12,13 +12,33 @@
         alt="OgbeniHMMD"
       />
       <div class="display-4 my-3" style=" ">Olajide A. Hammed</div>
-      <div class="h2 my-3">Freelancer, Front-end web developer...</div>
+      <div class="h2 mt-3">Freelancer &mdash; Frontend web developer</div>
     </div>
 
-    <div class="h3 p-1 m-0">
-      <a class="btn btn -lg btn-outline-primary px-md-5 mr-3 mr-md-5" href="#about">About</a>
-      <a class="btn btn-lg btn-outline-primary mr-3 mr-md-5" href="#social">Contact</a>
-      <a class="btn btn -lg btn-outline-primary px-md-5" href="/blog">Blog</a>
+    <div class="h1 p-1 m-1">
+      <a href="#about" title="About">
+        <i class="fa fa-info-circle px-4 py-3"></i>
+      </a>
+
+      <a href="#contact" title="Contact">
+        <i class="fa fa-envelope px-4 py-3"></i>
+      </a>
+
+      <a href="/blog" title="Blog">
+        <i class="fa fa-newspaper-o px-4 py-3"></i>
+      </a>
+
+      <a href="//t.me/OgbeniHMMD" title="Telegram">
+        <i class="fa fa-telegram px-4 py-3"></i>
+      </a>
+
+      <a href="//twitter.com/OgbeniHMMD" title="Twitter">
+        <i class="fa fa-twitter px-4 py-3"></i>
+      </a>
+
+      <a href="//github.com/OgbeniHMMD" title="Github">
+        <i class="fa fa-github px-4 py-3"></i>
+      </a>
     </div>
   </div>
 </template>
