33760e93241cd07a141b16480992f9d5643b5452
-h <Olajide.A.Hammed@gmail.com>
2020-05-07 16:47:06 +0100

    Added '/resume'

---
+++ pages/resume.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="p-4 min-vh-100" style="background-image: url('/patterns/stardust.png')">
+    <div class="container border border-primary">
+      <div class="row">
+        <aside class="col-4 bg-primary p-0">
+          <div class="bg-primary text-white border-bottom border-white px-4 py-5">
+            <div class="text-center">
+              <img
+                id="photo"
+                src="~/assets/img/OgbeniHMMD.jpg"
+                class="img-fluid rounded-circle border border-white"
+                alt="OgbeniHMMD"
+              />
+            </div>
+            <div class="py-3">
+              <i class="fa fa-phone mr-4"></i> +234 (0)810 092 8503
+            </div>
+            <div class="py-3">
+              <i class="fa fa-envelope mr-4"></i> olajide.a.hammed@gmail.com
+            </div>
+            <div class="py-3">
+              <a class="text-white" href="//github.ccom/OgbeniHMMD">
+                <i class="fa fa-map-marker mr-4"></i> Lagos, Nigeria
+              </a>
+            </div>
+            <div class="py-3">
+              <a class="text-white" href="/">
+                <i class="fa fa-link mr-4"></i> https://www.hmmd.xyz
+              </a>
+            </div>
+            <div class="py-3">
+              <a class="text-white" href="//github.ccom/OgbeniHMMD">
+                <i class="fa fa-github mr-4"></i> https://github.com/OgbeniHMMD
+              </a>
+            </div>
+          </div>
+
+          <div class="bg-primary text-white px-4 py-5 mb-5">
+            <div class="h1 mb-4">
+              <i class="fa fa-wrench mr-3"></i> SKILLS
+            </div>
+            <div class="py-3">
+              <i class="fa fa-asterisk mr-3"></i> HTML/CSS
+            </div>
+            <div class="py-3">
+              <i class="fa fa-asterisk mr-3"></i> JAVASCRIPT
+            </div>
+            <div class="py-3">
+              <i class="fa fa-asterisk mr-3"></i> VUE.JS/ NUXT.JS
+            </div>
+            <div class="py-3">
+              <i class="fa fa-asterisk mr-3"></i> VUE.JS/ NUXT.JS
+            </div>
+          </div>
+        </aside>
+
+        <main class="col-8 bg-white text-primary p-0">
+          <div class="p-5">
+            <div class="display-3">Olajide A. Hammed</div>
+            <div class="h2">Frontend Web Developer</div>
+          </div>
+
+          <div class="p-5 border-top border-primary">
+            <div class="d-flex my-4 w-100">
+              <div>
+                <i class="fa fa-user h1 mr-4"></i>
+              </div>
+              <div class="w-100">
+                <div class="h1">PROFILE</div>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-5 border-top border-primary">
+            <div class="d-flex my-4">
+              <div>
+                <i class="fa fa-graduation-cap h1 mr-4"></i>
+              </div>
+              <div class="w-100">
+                <div class="h1">EDUCATION</div>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-5 border-top border-primary">
+            <div class="d-flex my-4">
+              <div>
+                <i class="fa fa-paint-brush h1 mr-4"></i>
+              </div>
+              <div class="w-100">
+                <div class="h1">INTERESTS</div>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+                <p>Bla bla bla bla bla bla bla bla bla</p>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style >
+* {
+  //outline: 1px solid tomato;
+}
+</style>
