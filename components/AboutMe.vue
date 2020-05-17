143e43d4589fed9c5a4bd5b9c6516221ff65d232
-h <Olajide.A.Hammed@gmail.com>
2020-05-17 14:25:47 +0100

    Updated 'about' texts

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -4,19 +4,20 @@
     class="d-flex justify-content-center align-items-center min-vh-100 border-dark border-bottom m-0 my-auto"
     style="background-image: url('/patterns/asfalt-dark.png')"
   >
-    <div class="p-3 p-md-5">
-      <p class="h2 mb-4">Hello!</p>
-      <p class="h2 mb-4">
-        I am a hobbiest — freelance — javascript — frontend web developer. I build web apps usually with the JAMstack,
+    <div class="h2 p-3 p-md-5">
+      <p class="mb-4">Hello!</p>
+      <p class="mb-4">
+        I am a freelance — javascript — frontend web developer. I build web apps usually with the JAMstack,
         I.e, Javascript (vanilla JS, ES6, Vue.js), the markup languages and/or frameworks (Html/CSS, Bootstrap), and maybe some Everything-as-a-Service (Xaas) solutions.
       </p>
-      <p class="h2 mb-0">
-        If you are a business seeking a web presence, or you're an employer looking to hire, you can
-        <a
-          href="#contact"
-          class="font-weight-bold"
-        >contact me here</a>, or email me at
-        <a href="mailto:Ogbeni@hmmd.xyz">Ogbeni@hmmd.xyz</a>
+      <p>
+        Have a glimpse of
+        <a href="#works">my works</a> and
+        <a href="#contact">contact me here</a>, or
+        <a href="mailto:Ogbeni@hmmd.xyz">via E-mail</a>.
+      </p>
+      <p class="mb-0">
+        <i class="fa fa-smile-o"></i>
       </p>
     </div>
   </div>
--- pages/resume.vue
+++ pages/resume.vue
@@ -70,12 +70,12 @@
                 <div class="h1">PROFILE</div>
                 <div class="pb-4">
                   <p>
-                    I am a hobbiest &mdash; freelance &mdash; javascript &mdash; frontend web developer. I build web apps usually with the JAMstack,
+                    I am a freelance &mdash; javascript &mdash; frontend web developer. I build web apps usually with the JAMstack,
                     I.e, Javascript (vanilla JS, ES6, Vue.js), the markup languages and/or frameworks (Html/CSS, Bootstrap), and maybe some Everything-as-a-Service (Xaas) solutions.
                   </p>
                   <p>I have histories with ASP.NET/VB.NET, Java(SE), Python, PHP (Code Igniter, Laravel), Wordpress...</p>
                   <p>I don't crumble under pressure, and I'm ever willing to learn new technologies.</p>
-                  <p>I'm looking for a role of a junior web developer in a real company</p>
+                  <p>I'm looking for the role of a junior web developer.</p>
                 </div>
               </div>
             </div>
