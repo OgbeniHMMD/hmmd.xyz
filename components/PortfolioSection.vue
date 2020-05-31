2415273071998e5eb606cb2e82499fdd68a03f37
-h <Olajide.A.Hammed@gmail.com>
2020-05-31 13:55:51 +0100

    Added title and formatted the text

--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -1,12 +1,14 @@
 <template>
   <section class="min-vh-100 border-bottom m-0 my-auto">
     <div class="p-3 p-md-5 lead">
+      <div class="h1 mb-4">Portfolio</div>
+
       <div class="row mb-4">
         <div v-for="n in 6" class="col-12 col-sm-6 p-2">
           <div class="card border-0">
             <img src="/thumb.jpg" class="card-img-top" alt="..." />
             <div class="card-body p-2">
-              <p class="card-text">The five boxing wizards jump quickly.</p>
+              <p class="card-text text-muted">The five boxing wizards jump quickly.</p>
             </div>
           </div>
         </div>
