46ab96bb79462fc664bae8cf3332414ae1124c03
-h <Olajide.A.Hammed@gmail.com>
2020-06-21 16:31:14 +0100

    fix icon issues

--- components/IntroSection.vue
+++ components/IntroSection.vue
@@ -1,24 +1,24 @@
 <template>
-  <section class="d-flex flex-column justify-content-around min-vh-100 border-bottom m-0 my-auto">
-    <div class="p-3 p-md-5">
-      <p class="display-4 pb-5 mb-5 border-primary border-bottom">Frontend web developer</p>
+	<section class="d-flex flex-column justify-content-around min-vh-100 border-bottom m-0 my-auto">
+		<div class="p-3 p-md-5">
+			<p class="display-4 pb-5 mb-5 border-primary border-bottom">Frontend web developer</p>
 
-      <p class="h2 mb-5">
-        I build web apps usually with the JAMstack.
-        <br />#Javascript #Vue.js #Nuxt.js
-      </p>
-    </div>
+			<p class="h2 mb-5">
+				I build web apps usually with the JAMstack.
+				<br />#Javascript #Vue.js #Nuxt.js #bootstrap
+			</p>
+		</div>
 
-    <div class="d-flex justify-content-around h2 mb-0 p-1">
-      <a href="#portfolio" title="Portfolio">
-        <i class="fa fa-suitcase text-dark p-2 p-sm-3"></i>Portfolio
-      </a>
-      <a href="#contact" title="Contact me">
-        <i class="fa fa-envelope text-dark p-2 p-sm-3"></i>Contact
-      </a>
-      <a href="/blog" title="Blog">
-        <i class="fa fa-newspaper text-dark p-2 p-sm-3"></i>Blog
-      </a>
-    </div>
-  </section>
+		<div class="d-flex justify-content-around h2 mb-0 p-1">
+			<a href="#portfolio" title="Portfolio">
+				<i class="fa fa-suitcase text-dark p-2 p-sm-3"></i>Portfolio
+			</a>
+			<a href="#contact" title="Contact me">
+				<i class="fa fa-envelope text-dark p-2 p-sm-3"></i>Contact
+			</a>
+			<a href="/blog" title="Blog">
+				<i class="fa fa-newspaper text-dark p-2 p-sm-3"></i>Blog
+			</a>
+		</div>
+	</section>
 </template>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,33 +1,33 @@
 <template>
-  <section
-    class="d-flex justify-content-center align-items-center flex-column min-vh-100 m-0 my-auto"
-  >
-    <div class="w-100 text-center text-primary p-2">
-      <img
-        id="photo"
-        src="~/assets/img/OgbeniHMMD.jpg"
-        class="img-fluid rounded-circle border border-primary"
-        alt="OgbeniHMMD"
-      />
-      <div class="display-4 my-3" style=" ">Olajide A. Hammed</div>
+	<section
+		class="d-flex justify-content-center align-items-center flex-column min-vh-100 m-0 my-auto"
+	>
+		<div class="w-100 text-center text-primary p-2">
+			<img
+				id="photo"
+				src="~/assets/img/OgbeniHMMD.jpg"
+				class="img-fluid rounded-circle border border-primary"
+				alt="OgbeniHMMD"
+			/>
+			<div class="display-4 my-3" style=" ">Olajide A. Hammed</div>
 
-      <div class="d-flex justify-content-around h1 mb-0 p-1">
-        <a href="#intro" class="d-lg-none">
-          <i class="fa fa-chevron-circle-down text-dark bounce p-2 p-sm-3"></i>
-        </a>
+			<div class="d-flex justify-content-around h1 mb-0 p-1">
+				<a href="#intro" class="d-lg-none">
+					<i class="fas fa-chevron-circle-down text-dark bounce p-2 p-sm-3"></i>
+				</a>
 
-        <a href="//t.me/OgbeniHMMD" title="Telegram">
-          <i class="fa fa-telegram p-2 p-sm-3"></i>
-        </a>
+				<a href="//t.me/OgbeniHMMD" title="Telegram">
+					<i class="fab fa-telegram p-2 p-sm-3"></i>
+				</a>
 
-        <a href="//twitter.com/OgbeniHMMD" title="Twitter">
-          <i class="fa fa-twitter p-2 p-sm-3"></i>
-        </a>
+				<a href="//twitter.com/OgbeniHMMD" title="Twitter">
+					<i class="fab fa-twitter p-2 p-sm-3"></i>
+				</a>
 
-        <a href="//github.com/OgbeniHMMD" title="Github">
-          <i class="fa fa-github p-2 p-sm-3"></i>
-        </a>
-      </div>
-    </div>
-  </section>
+				<a href="//github.com/OgbeniHMMD" title="Github">
+					<i class="fab fa-github p-2 p-sm-3"></i>
+				</a>
+			</div>
+		</div>
+	</section>
 </template>
