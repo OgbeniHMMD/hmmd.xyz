06b22e02a22704b72710ded6604146576f19d44d
-h <Olajide.A.Hammed@gmail.com>
2020-07-10 02:57:12 +0100

    Reduce box shadow

--- components/TheQuote.vue
+++ components/TheQuote.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="shadow mb-3 bg-dark jumbotron rounded-0">
+	<div class="shadow-sm mb-3 bg-dark jumbotron rounded-0">
 		<div class="container text-center text-light lead">
 			<p>“The Most Comfortable thing is to live within your means and damn the consequences...”</p>—
 			Muhammadu Buhari
