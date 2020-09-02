d1017fb7a9e0bfe4e6673dce267199ac73a5415a
Olajide A. Hammed <olajide.a.hammed@gmail.com>
2020-09-02 09:47:12 +0100

    Commit 0.2.8

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-	<div>
-		<header-section id="top" />
-		<quote-section id="quote" />
-		<portfolio-section id="portfolio" />
-		<contact-section id="contact" />
-		<footer-section id="footer" />
-	</div>
+  <div>
+    <header-section id="top" />
+    <quote-section id="quote" />
+    <portfolio-section id="portfolio" />
+    <contact-section id="contact" />
+    <footer-section id="footer" />
+  </div>
 </template>
 
 
@@ -17,12 +17,16 @@ import ContactSection from "~/components/ContactSection.vue";
 import FooterSection from "~/components/FooterSection.vue";
 
 export default {
-	components: {
-		HeaderSection,
-		QuoteSection,
-		PortfolioSection,
-		ContactSection,
-		FooterSection
-	}
+  components: {
+    HeaderSection,
+    QuoteSection,
+    PortfolioSection,
+    ContactSection,
+    FooterSection,
+  },
+
+  head: {
+    title: "OgbeniHMMD — hmmd.xyz",
+  },
 };
 </script>
