5cab0f0aee92d20d90345884b0c4454c0a29e169
OgbeniHMMD <olajide.a.hammed@gmail.com>
2020-12-30 04:26:36 +0100

    Improved Code

--- components/resume/ResumeSidebar.vue
+++ components/resume/ResumeSidebar.vue
@@ -1,68 +1,85 @@
 <template>
   <aside class="col-4 bg-primary p-0">
-    <div class="bg-primary text-white border-bottom border-white px-3 py-5">
+    <section class="bg-primary text-white border-bottom border-white px-3 py-5">
       <div class="text-center">
         <img
           id="photo"
           src="~/assets/img/OgbeniHMMD.jpg"
           class="img-fluid rounded-circle border border-white"
-          alt="OgbeniHMMD"
+          alt="OgbeniHMMD Head Shot"
         />
       </div>
-      <div class="py-3 text-break">
-        <i class="fas fa-phone fa-pull-left mr-3"></i>
-        <a href="tel:+2348100928503" class="text-white text-decoration-none"
-          >+234 (0)810 092 8503</a
-        >
-      </div>
-      <div class="py-3 text-break">
-        <i class="fas fa-envelope fa-pull-left mr-3"></i>
-        <a
-          href="mailto:Olajide.A.Hammed@gmail.com"
-          class="text-white text-decoration-none"
-          >Olajide.A.Hammed@gmail.com</a
-        >
-      </div>
-      <div class="py-3 text-break">
-        <i class="fas fa-map-marker fa-pull-left mr-4"></i> Lagos, Nigeria
-      </div>
-      <div class="py-3 text-break">
-        <a class="text-white text-decoration-none" href="/">
-          <i class="fa fa-link mr-3"></i> www.hmmd.xyz
-        </a>
-      </div>
-      <div class="py-3 text-break">
-        <a
-          class="text-white text-decoration-none"
-          href="//github.com/OgbeniHMMD"
-        >
-          <i class="fab fa-linkedin mr-3"></i>
-          <span>LinkedIn.com/OgbeniHMMD</span>
-        </a>
-      </div>
-      <div class="py-3 text-break">
-        <a
-          class="text-white text-decoration-none"
-          href="//github.com/OgbeniHMMD"
-        >
-          <i class="fab fa-github mr-3"></i> Github.com/OgbeniHMMD
-        </a>
-      </div>
-    </div>
 
-    <div class="bg-primary text-white px-4 py-5 mb-5">
-      <div class="h1 mb-4"><i class="fas fa-tools mr-3"></i> SKILLS</div>
-      <div class="py-3"><i class="fas fa-code mr-3"></i> HTML5 / CSS3</div>
-      <div class="py-3"><i class="fab fa-css3 mr-3"></i> SCSS / PostCSS</div>
-      <div class="py-3"><i class="fab fa-js-square mr-3"></i> Javascript</div>
-      <div class="py-3"><i class="fab fa-js-square mr-3"></i> Typescript</div>
-      <div class="py-3"><i class="fab fa-vuejs mr-3"></i> Vue.js / Nuxt.js</div>
-      <div class="py-3">
-        <i class="fab fa-react mr-3"></i> React.js / Gastby / Next.js
+      <div
+        class="py-3 text-break"
+        v-for="contact in contacts"
+        :key="contact.icon"
+      >
+        <i class="fa-pull-left mr-3" :class="contact.icon"></i>
+        <a
+          :href="contact.href"
+          class="text-white text-decoration-none"
+          v-text="contact.title"
+        ></a>
       </div>
-      <div class="py-3"><i class="fab fa-git mr-3"></i> Git / Github</div>
-      <div class="py-3"><i class="fab fa-linux mr-3"></i> Linux / CLI</div>
-      <div class="py-3"><i class="fab fa-aws mr-3"></i> Netlify / AWS</div>
-    </div>
+    </section>
+
+    <section class="bg-primary text-white px-4 py-5 mb-5">
+      <div class="h1 mb-4"><i class="fas fa-tools mr-3"></i> SKILLS</div>
+      <div class="py-3" v-for="skill in skills" :key="skill.title">
+        <i class="mr-3" :class="skill.icon"></i>
+        <span v-text="skill.title"></span>
+      </div>
+    </section>
   </aside>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: [
+        {
+          title: "+234 (0)810 092 8503",
+          href: "tel:+2348100928503",
+          icon: "fas fa-phone",
+        },
+        {
+          title: "Olajide.A.Hammed@gmail.com",
+          href: "mailto:Olajide.A.Hammed@gmail.com",
+          icon: "fas fa-envelope",
+        },
+        {
+          title: "Lagos, Nigeria",
+          href: "http://maps.google.com/?q=Lagos+Nigeria",
+          icon: "fas fa-map-marker",
+        },
+        { title: "www.hmmd.xyz", href: "/", icon: "fa fa-link" },
+        {
+          title: "LinkedIn.com/OgbeniHMMD",
+          href: "https://www.linkedin.com/in/ogbenihmmd/",
+          icon: "fab fa-linkedin",
+        },
+        {
+          title: "Github.com/OgbeniHMMD",
+          href: "https://github.com/OgbeniHMMD",
+          icon: "fab fa-github",
+        },
+        { title: "", href: "", icon: "" },
+      ],
+
+      skills: [
+        { title: "HTML5 / CSS3", icon: "fas fa-code" },
+        { title: "SCSS / PostCSS", icon: "fab fa-css3" },
+        { title: "Javascript", icon: "fab fa-js-square " },
+        { title: "Typescript", icon: "fab fa-js-square " },
+        { title: "Vue.js / Vuex & Nuxt.js", icon: "fab fa-vuejs" },
+        { title: "React.js / Redux  & Gastby", icon: "fab fa-react" },
+        { title: "Git / Github", icon: "fab fa-git" },
+        { title: "Linux / CLI", icon: "fab fa-linux" },
+        { title: "Netlify / AWS", icon: "fab fa-aws" },
+      ],
+    };
+  },
+};
+</script>
